3ed3fa86dcbc2a8796e1b7930a2474ee6378159b
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-09-26 05:37:57 +0600

    title and favicon changed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Fundraising for Environment
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="app-wrapper">
-      
+      <header>
+        <h1>Fundraising for Environment</h1>
+        <p>Building a strong resistance to protect the Environment before it's too late. It's now or never.</p>
+        <h2>Project Fund: 5 million</h2>
+      </header>
     </div>
   );
 }
